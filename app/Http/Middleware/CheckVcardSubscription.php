204de2e1118b94c4bf1ba2ab8e83d3eaa6f79fd4
suriya1607 <suriya1672001@gmail.com>
2025-09-17 22:05:59 +0530

    subscription expiry removed vcard profile

--- app/Http/Middleware/CheckVcardSubscription.php
+++ app/Http/Middleware/CheckVcardSubscription.php
@@ -27,7 +27,7 @@ class CheckVcardSubscription
         if ($user->subscription->ends_at > Carbon::now()->format('Y-m-d H:i:s')) {
             return $request;
         }
-
+        return $request;
         return abort(404);
     }
 }
